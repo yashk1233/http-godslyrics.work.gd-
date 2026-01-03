79dc8f68481ef64b034cd5f1403d444e068a5185
Yash <kakadeyash224@gmail.com>
2026-01-03 22:57:23 +0530

    header title change

--- laravel/resources/views/header.blade.php
+++ laravel/resources/views/header.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf_token" content="{{ csrf_token() }}" />
-  <title>Dashboard</title>
+  <title>GodsLyrics</title>
 
   <!-- Google Font: Source Sans Pro -->
   <!-- <link rel="stylesheet"
